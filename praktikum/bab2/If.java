ff37eb73ee9e02e2aca7da501fc3cfc568751587
Yoga Raditya <radityanalaa@gmail.com>
2024-09-24 20:34:21 +0700

    `feat`: Add If example

---
+++ praktikum/bab2/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        int nilai = 80;
+
+        if (nilai > 80) {
+            // jika nilai di atas 80, maka print lulus
+            System.out.println("Lulus");
+        }
+
+        // jika tidak, maka tidak melakukan apapun
+    }
+}
