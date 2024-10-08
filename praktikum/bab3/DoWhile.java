5c63ae6411345889f2d26b4888787a383c7c2369
Yoga Raditya <radityanalaa@gmail.com>
2024-10-08 13:57:10 +0700

    `feat`: Add praktikum bab 3

--- praktikum/bab3/DoWhile.java
+++ praktikum/bab3/DoWhile.java
@@ -1,3 +1,39 @@
+import java.util.*;
+
 public class DoWhile {
-    
+    public static void main(String[] args) {
+        /*
+         * do {
+         *      statement1;
+         *      statement2;
+         *      ...
+         *      statementN;
+         * } while (kondisi);
+         */
+
+        // instansiasi Scanner 
+        Scanner input = new Scanner(System.in);
+
+        // konstanta PI untuk menghitung luas lingkaran
+        final double PI = 3.14;
+
+        // inisialisasi jari-jari dengan variable r
+        double r;
+
+        // program akan menjalankan bagian do setidaknya satu kali
+        do {
+            // meminta input dari user
+            r = input.nextDouble();
+
+            // jika r < 0, print pesan invalid
+            if (r < 0) {
+                System.out.println("input invalid, masukkan nilai baru");
+            }
+        } while (r < 0); 
+        // jika r < 0 bernilai true, jalankan lagi do
+        // jika false, lanjut eksekusi kode di bawah
+
+        // print luas lingkaran
+        System.out.println(PI * r * r);
+    }
 }
--- praktikum/bab3/While.java
+++ praktikum/bab3/While.java
@@ -1,3 +1,37 @@
+import java.util.*;
+
 public class While {
-    
+    public static void main(String[] args) {
+        /*
+         * while (kondisi) {
+         *      statement1;
+         *      statement2;
+         *      ...
+         *      statementN;
+         * }
+         */
+
+        // instansiasi Scanner 
+        Scanner input = new Scanner(System.in);
+
+        // konstanta PI untuk menghitung luas lingkaran
+        final double PI = 3.14;
+
+        // meminta input jari-jari (r) dari user
+        double r = input.nextDouble();
+
+        // looping akan terjadi selama nilai r < 0
+        while (r < 0) {
+            System.out.println("input invalid, masukkan nilai baru");
+
+            // program akan meminta input r yang baru
+            r = input.nextDouble();
+
+            // apabila r < 0, looping akan terjadi lagi
+            // apabila r >= 0, looping akan berhenti
+        }
+
+        // print luas lingkaran
+        System.out.println(PI * r * r);
+    }
 }
