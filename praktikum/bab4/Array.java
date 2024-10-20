69d9f6b24bddd9c5c980e225a5cf8dde2bd673e7
Yoga Raditya <radityanalaa@gmail.com>
2024-10-20 21:01:55 +0700

    `feat`: Add array code example

---
+++ praktikum/bab4/Array.java
@@ -0,0 +1,21 @@
+public class Array {
+    public static void main(String[] args) {
+        // deklarasi array
+        int[] nilaiAkhir;
+
+        // instansiasi array
+        nilaiAkhir = new int[10];
+
+        // deklarasi & instansiasi array
+        int[] nilaiPemdas = new int[10];
+
+        // inisialisasi nilai array
+        nilaiPemdas[0] = 90;
+        nilaiPemdas[1] = 100;
+        nilaiPemdas[2] = 98;
+        nilaiPemdas[9] = 99;
+
+        // array initializers
+        int[] nilaiRerata = {100, 98, 97, 99, 10};
+    }
+}
